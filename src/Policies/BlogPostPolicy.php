50f2e35587105639a8784db2dcdb53cecd6c8c75
David <kontakt@david-grass-feria.de>
2024-03-22 22:01:47 +0100

    Update BlogPostPolicy.php

--- src/Policies/BlogPostPolicy.php
+++ src/Policies/BlogPostPolicy.php
@@ -3,6 +3,7 @@
 namespace GrassFeria\StarterkidBlog\Policies;
 
 use App\Models\User;
+use GrassFeria\Starterkid\Traits\EditorPolicyTrait;
 use \GrassFeria\StarterkidBlog\Models\BlogPost;
 use Illuminate\Auth\Access\Response;
 use GrassFeria\Starterkid\Traits\OnlyUserRecordPolicyTrait;;
@@ -10,7 +11,7 @@ use GrassFeria\Starterkid\Traits\OnlyUserRecordPolicyTrait;;
 class BlogPostPolicy
 {
    
-     use OnlyUserRecordPolicyTrait;
+     use EditorPolicyTrait;
 
    
 
