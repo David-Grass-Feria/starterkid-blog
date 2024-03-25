903193a0702d2a32b43b81595eb5b5ab1f86fe8c
David <kontakt@david-grass-feria.de>
2024-03-25 15:05:44 +0100

    Update front-blog-post-index.blade.php

--- resources/views/livewire/front/blog-post/front-blog-post-index.blade.php
+++ resources/views/livewire/front/blog-post/front-blog-post-index.blade.php
@@ -24,49 +24,26 @@
             {{$blogposts->links()}}
         </x-slot>
     
-        <div>
-            <div class="mx-auto">
-            
-              <div class="mx-auto mt-5 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+       
                 
-                
+                <x-starterkid-frontend::card-grid>
                 @foreach($blogposts as $blogpost)
-                <article class="flex flex-col items-start justify-between bg-white rounded-3xl border border-gray-400">
-                  <div class="relative w-full">
-                    @if (!empty($blogpost->getFirstMediaUrl('images', 'medium')))
-                    <img src="{{$blogpost->getFirstMediaUrl('images','medium')}}" alt="{{$blogpost->name}}" class="w-full object-cover rounded-t-3xl aspect-[2/2] lg:aspect-[3/3]">
-                    
-                    @else
-                    <img src="{{Cache::has('logo') ? Cache::get('logo') : asset('/logo.png')}}" alt="{{$blogpost->name}}" class="w-full object-contain rounded-t-3xl aspect-[2/2] lg:aspect-[3/3]">
-                    @endif
-                    
-                    <x-starterkid-frontend::image-credits imageCredits="{{$blogpost->image_credits}}"/>
-                  </div>
-                  <div class="w-full text-font_primary p-3">
-                    <div class="mt-8 flex items-center gap-x-4 text-xs">
-                      <time datetime="{{$blogpost->getPublished()}}">{{$blogpost->getPublished()}}</time>
-                      <p>{{$blogpost->author}}</p>
-                    </div>
-                    <div class="group relative">
-                      <h3 class="mt-3 text-lg font-semibold">
-                        <a href="{{route('front.blog-post.show',$blogpost->slug)}}">
-                          <span class="absolute inset-0"></span>
-                          {{$blogpost->name}}
-                        </a>
-                      </h3>
-                      <p class="mt-5 line-clamp-3 text-sm">{!!Str::limit($blogpost->preview,200)!!}</p>
-                    </div>
-                   
-                  </div>
-                </article>
+               <x-starterkid-frontend::card-grid-blogpost-item 
+               imgSrc="{{$blogpost->getFirstMediaUrl('images', 'medium')}}" 
+               imgAlt="{{$blogpost->name}}" 
+               imgCredits="{{$blogpost->image_credits}}" 
+               dateTime="{{$blogpost->getPublished()}}" 
+               author="{{$blogpost->author}}" 
+               heading="{{$blogpost->name}}" 
+               preview="{!!Str::limit($blogpost->preview,200)!!}" 
+               href="{{route('front.blog-post.show',$blogpost->slug)}}" 
+               hrefTitle="{{$blogpost->name}}" />
                 @endforeach
                 
-                
+              </x-starterkid-frontend::card-grid>
 
                  
-              </div>
-            </div>
-          </div>
+         
           
           
 
