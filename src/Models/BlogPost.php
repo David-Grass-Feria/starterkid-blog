588cfa7beeb82dcde6a8ee8120a51b1ba0111692
David <kontakt@david-grass-feria.de>
2024-03-28 19:55:38 +0100

    Update BlogPost.php

--- src/Models/BlogPost.php
+++ src/Models/BlogPost.php
@@ -110,7 +110,7 @@ class BlogPost extends Model implements HasMedia
         $this->addMediaConversion('thumb')
               ->width(200);
         $this->addMediaConversion('medium')
-              ->width(500);
+              ->width(400);
         $this->addMediaConversion('large')
               ->width(1000);
         
