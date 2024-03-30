5eac227aa4e2377eaa4d1c024da93d03faa116aa
David <kontakt@david-grass-feria.de>
2024-03-30 23:07:01 +0100

    Update starterkid-blog.php

--- config/starterkid-blog.php
+++ config/starterkid-blog.php
@@ -5,5 +5,5 @@ return [
     'blog_post_slug'                 => env('BLOG_POST_SLUG','blog'),
     'blog_post_title'                => env('BLOG_POST_TITLE','Blog'),
     'blog_post_description'          => env('BLOG_POST_DESCRIPTION','Aktuelle Beiträge aus Saalfeld und unserem Stadtrat'),
-    'blog_post_pagination'           => env('BLOG_POST_PAGINATION',6),
+    'blog_post_pagination'           => env('BLOG_POST_PAGINATION',3),
 ];
