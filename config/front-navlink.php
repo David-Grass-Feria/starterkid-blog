9787d97f32c8817e16ccfe06668d79b967106d15
David <kontakt@david-grass-feria.de>
2024-04-23 20:54:22 +0200

    1.0

--- config/front-navlink.php
+++ config/front-navlink.php
@@ -2,7 +2,7 @@
 
 return [
     [
-        'title' => env('BLOG_POST_TITLE','Blog'),
+        'title' => env('BLOG_POST_MENU_NAME','Blog'),
         'route' => 'front.blog-post.index',
         'active' => ['front.blog-post.index','front.blog-post.show'],
         'order' => env('BLOG_ORDER_FOR_NAV',1),
